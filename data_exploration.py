ce271805e44100e83b4cfd134c741331ce5bc3d0
maringow <mgow@westmonroepartners.com>
2019-05-10 13:46:31 -0500

    data exploration

--- data_exploration.py
+++ data_exploration.py
@@ -1,19 +1,29 @@
 import pandas as pd
 import numpy as np
 import os
+import sklearn
+from sklearn.cluster import KMeans
 
 df = pd.read_csv('hospitalData.csv')
 
-print(df.columns)
-
 # run K-means clustering within survey results and outcomes - doesn't make sense to compare across the two
-outcomes = df[['Provider ID', 'COMP_HIP_KNEE', 'MORT_30_AMI', 'MORT_30_CABG', 'MORT_30_COPD', 'MORT_30_HF', 'MORT_30_PN',
+
+# build outcome and survey dataframes and fill NaNs with mean values
+outcomes = df[['COMP_HIP_KNEE', 'MORT_30_AMI', 'MORT_30_CABG', 'MORT_30_COPD', 'MORT_30_HF', 'MORT_30_PN',
               'MORT_30_STK', 'PSI_10_POST_KIDNEY', 'PSI_11_POST_RESP', 'PSI_12_POSTOP_PULMEMB_DVT', 'PSI_13_POST_SEPSIS',
               'PSI_14_POSTOP_DEHIS', 'PSI_15_ACC_LAC', 'PSI_3_ULCER', 'PSI_4_SURG_COMP', 'PSI_6_IAT_PTX',
               'PSI_8_POST_HIP', 'PSI_90_SAFETY', 'PSI_9_POST_HEM']]
+outcomes2 = outcomes.fillna(outcomes.mean(), inplace=True)
+print(outcomes.mean)
+
+# outcomes2.to_csv(path_or_buf='C:/Users/mgow/Documents/UChicago/Courses/Data Mining/Group Project/outcomesFilled.csv')
 surveys = df[['Provider ID', 'H_CLEAN_LINEAR_SCORE', 'H_COMP_1_LINEAR_SCORE', 'H_COMP_2_LINEAR_SCORE', 'H_COMP_3_LINEAR_SCORE',
               'H_COMP_5_LINEAR_SCORE', 'H_COMP_6_LINEAR_SCORE', 'H_COMP_7_LINEAR_SCORE', 'H_HSP_RATING_LINEAR_SCORE',
-              'H_QUIET_LINEAR_SCORE', 'H_RECMND_LINEAR_SCORE']]
+              'H_QUIET_LINEAR_SCORE', 'H_RECMND_LINEAR_SCORE']].values
+
+# model=KMeans(n_clusters=5, random_state=0)
+# model.fit_predict(outcomes)
+
 
 
 
